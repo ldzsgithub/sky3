caa67575031b818af35ed743cbe0282b063d3b64
ldz <l1004834830@163.com>
2020-02-28 19:25:15 +0800

    0228hm

--- src/Login.vue
+++ src/Login.vue
@@ -6,25 +6,25 @@
 </template>
 
 <script>
-  export default {
-    name: 'login',
-    components: {
-    },
-    methods: {
-      login() {
-        const redirect = this.$route.query.redirect
-        if(redirect){
-          //存在回跳地址就回跳
-          this.$router.push(redirect)
-        }else{
-          //否则就跳到默认的首页
-          this.$router.push({
-            name: 'Home'
-          })
-        }
-      }
-    }
-  }
+  // export default {
+  //   name: 'login',
+  //   components: {
+  //   },
+  //   methods: {
+  //     login() {
+  //       const redirect = this.$route.query.redirect
+  //       if(redirect){
+  //         //存在回跳地址就回跳
+  //         this.$router.push(redirect)
+  //       }else{
+  //         //否则就跳到默认的首页
+  //         this.$router.push({
+  //           name: 'Home'
+  //         })
+  //       }
+  //     }
+  //   }
+  // }
 </script>
 
 <style>
--- src/router/index.js
+++ src/router/index.js
@@ -40,23 +40,23 @@ const router = new VueRouter({
   routes
 })
 
-router.beforeEach((to, from, next) => {
-  //用你的方式获取登录的用户信息
-  const userInfo = localStorage.userinfo
-  if(userInfo || to.name === 'Login'){
-    //如果存在用户信息，或者进入的页面是登录页面，则直接进入
-    next()
-  }else {
-    //不存在用户信息则说明用户未登录，跳转到登录页面，带上当前的页面地址，登录完成后做回跳，
-    //如未登录用户进入用户中心的页面地址，检测到未登录，
-    //自动跳转到登录页面，并且带上用户中心的页面地址，登录完成后重新跳到个人中心页面。
-    next({
-      name: 'Login',
-      query: {
-        redirect: to.path
-      }
-    })
-  }
-})
+// router.beforeEach((to, from, next) => {
+//   //用你的方式获取登录的用户信息
+//   const userInfo = localStorage.userinfo
+//   if(userInfo || to.name === 'Login'){
+//     //如果存在用户信息，或者进入的页面是登录页面，则直接进入
+//     next()
+//   }else {
+//     //不存在用户信息则说明用户未登录，跳转到登录页面，带上当前的页面地址，登录完成后做回跳，
+//     //如未登录用户进入用户中心的页面地址，检测到未登录，
+//     //自动跳转到登录页面，并且带上用户中心的页面地址，登录完成后重新跳到个人中心页面。
+//     next({
+//       name: 'Login',
+//       query: {
+//         redirect: to.path
+//       }
+//     })
+//   }
+// })
 
 export default router
